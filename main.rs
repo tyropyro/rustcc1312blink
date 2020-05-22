fd5b75603a8735311b886fb991c33c463d83de8b
tyropyro <65506237+tyropyro@users.noreply.github.com>
2020-05-22 12:22:24 -0400

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,34 @@
+#![no_std]
+#![no_main]
+
+extern crate panic_halt;
+extern crate cortex_m_rt as rt;
+use panic_reset as _;
+
+use cc1312::Peripherals;
+
+use cortex_m_rt::entry;
+// use `main` as the entry point of this application
+// `main` is not allowed to return
+use std::ptr::{read_volatile, write_volatile};
+#[entry]
+
+fn main() -> ! {
+
+    const A: *mut u32 = ( + )
+
+    let p = Peripherals::take().unwrap();
+
+}
+
+fn inner_main() -> Result<(), &'static str> { 
+
+    loop {
+
+if {
+
+}
+
+    }
+
+}
